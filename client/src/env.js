dba6d313ba41af2279acf5b139a65f487d454d53
ABdulaziz <a.captain.b@gmail.com>
2024-07-23 15:08:16 -0600

    init

---
+++ client/src/env.js
@@ -0,0 +1,7 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-cucsliboqrmpiibf.us.auth0.com'
+export const clientId = '9IHCopOWsypgrZC9Nniolr8lpzQ6Xvig'
+export const audience = 'https://GregsList.com'
+
---
+++ server/www/env.js
@@ -0,0 +1,3 @@
+export const domain = 'dev-cucsliboqrmpiibf.us.auth0.com'
+export const clientId = '9IHCopOWsypgrZC9Nniolr8lpzQ6Xvig'
+export const audience = 'https://GregsList.com'
